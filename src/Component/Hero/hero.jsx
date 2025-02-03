666d6655653d75c6c54bffdff7debfb10bfddd44
sarimsaleem <sarimsaleem07@gmail.com>
2025-02-03 18:44:33 +0500

    small changes

--- src/Component/Hero/hero.jsx
+++ src/Component/Hero/hero.jsx
@@ -23,22 +23,35 @@ const HeroSection = () => {
     }, []);
 
     return (<>
-   
+
         <div className='hero-main' id='home'>
             <div className="container">
                 <div className="row align-items-center">
+                    <div className='sub-header-main'>
+                        <div className="container nav-upper-text res-none">
+                            <p className='nav-header-text'>
+                                Integrative Counsellor for Children, Young people and Adults. Online, by phone and in-person in Walthamstow, East London
+                            </p>
+                            <div className='nav-icon-parent'>
+                                <a href="tel:+447777966289">
+                                    <FaPhone className='nav-icons-header-phone' />
+                                </a>|
+                                <a href="mailto:cavecounselling@outlook.com">
+                                    <IoIosMail className='nav-icons-header-mail' />
+                                </a>
+                            </div>
+                        </div>
+                    </div>
                     <div className="col-md-7 col-sm-12 col-lg-7 col-xl-7">
                         <div className="hero-content">
                             <div className="hero-content-heading">
+
                                 <h4 className='p-0 m-0'>Welcome to</h4>
                                 <h1 className='p-0 m-0'>Cave Counselling</h1>
                             </div>
                             <p className='p-0 m-0'>
-                              " A sanctuary where you can escape the noise of the world and reconnect with your inner self. Amid the hustle and bustle of daily life, moments of stillness to reflect, explore, and find clarity are rare. Whether you’re navigating challenges, feeling isolated, or seeking inner peace, you don’t have to face life’s transitions or hardships alone. Let this be your space for understanding, healing, and discovering the strength within. "
+                                " A sanctuary where you can escape the noise of the world and reconnect with your inner self. Amid the hustle and bustle of daily life, moments of stillness to reflect, explore, and find clarity are rare. Whether you’re navigating challenges, feeling isolated, or seeking inner peace, you don’t have to face life’s transitions or hardships alone. Let this be your space for understanding, healing, and discovering the strength within. "
                             </p>
-                            {/* <div className="hero-content-btn">
-                                <button>LEARN MORE</button>
-                            </div> */}
                         </div>
                     </div>
                 </div>
@@ -50,28 +63,3 @@ const HeroSection = () => {
 }
 
 export default HeroSection;
-
-
-
-// const HeroSection = () => {
-//     return (
-//         <div className='hero-main' id='home'>
-//             <div className="container">
-//                 <div className="row align-items-center">
-//                     <div className="col-md-7 col-sm-12 col-lg-7 col-xl-7">
-//                         <div className="hero-content">
-//                             <div className="hero-content-heading">
-//                                 <h4 className='p-0 m-0'>Welcome to</h4>
-//                                 <h1 className='p-0 m-0'>Cave Counselling</h1>
-//                             </div>
-//                             <p className='p-0 m-0'>“A sanctuary where you can escape the noise of the world and reconnect with your inner self. Amid the hustle and bustle of daily life, moments of stillness to reflect, explore, and find clarity are rare. Whether you’re navigating challenges, feeling isolated, or seeking inner peace, you don’t have to face life’s transitions or hardships alone. Let this be your space for understanding, healing, and discovering the strength within.”</p>
-//                             <div className="hero-content-btn">
-//                                 <button>LEARN MORE</button>
-//                             </div>
-//                         </div>
-//                     </div>
-//                 </div>
-//             </div>
-//         </div>
-//     )
-// }
--- src/Component/Navbar/Navbar.jsx
+++ src/Component/Navbar/Navbar.jsx
@@ -51,21 +51,6 @@ function Header() {
           </div>
         </div>
       </nav>
-      <div className='sub-header-main'>
-        <div className="container nav-upper-text res-none">
-          <p className='nav-header-text'>
-            Integrative Counsellor for Children, Young people and Adults. Online, by phone and in-person in Walthamstow, East London
-          </p>
-          <div className='nav-icon-parent'>
-            <a href="tel:+447777966289">
-              <FaPhone className='nav-icons-header-phone' />
-            </a>|
-            <a href="mailto:cavecounselling@outlook.com">
-              <IoIosMail className='nav-icons-header-mail' />
-            </a>
-          </div>
-        </div>
-      </div>
     </div>
   );
 }
