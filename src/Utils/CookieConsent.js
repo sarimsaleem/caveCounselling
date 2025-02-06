55ae709d7c65d9521905860b1a4ea8f9883f2894
sarimsaleem <sarimsaleem07@gmail.com>
2025-02-06 18:11:26 +0500

    changes

--- src/Utils/CookieConsent.js
+++ src/Utils/CookieConsent.js
@@ -1,40 +1,47 @@
 import React, { useState, useEffect } from "react";
 import Cookies from "js-cookie";
 import "./cookieConsent.css"
+import { RxCookie } from "react-icons/rx";
 
 const CookieConsent = () => {
-  const [isVisible, setIsVisible] = useState(false);
+    const [isVisible, setIsVisible] = useState(false);
 
-  useEffect(() => {
-    const consent = Cookies.get("user");
-    const declined = localStorage.getItem("cookieDeclined");
+    useEffect(() => {
+        const consent = Cookies.get("user");
+        const declined = localStorage.getItem("cookieDeclined");
 
-    if (!consent && !declined) {
-      setIsVisible(true);
-    }
-  }, []);
+        if (!consent && !declined) {
+            setIsVisible(true);
+        }
+    }, []);
 
-  const acceptCookies = () => {
-    Cookies.set("user", "true", { expires: 365 });
-    setIsVisible(false);
-  };
+    const acceptCookies = () => {
+        Cookies.set("user", "true", { expires: 365 });
+        setIsVisible(false);
+    };
 
-  const declineCookies = () => {
-    localStorage.setItem("cookieDeclined", "true"); // Remember that the user declined
-    setIsVisible(false);
-  };
+    const declineCookies = () => {
+        localStorage.setItem("cookieDeclined", "true");
+        setIsVisible(false);
+    };
 
-  if (!isVisible) return null;
+    if (!isVisible) return null;
 
-  return (
-    <div className="cookieContainer">
-      <p className="cookie-text">This website uses cookies to enhance your experience.</p>
-      <div className="cookie-btn-container" >
-        <button onClick={acceptCookies} className="acceptBtn" >Accept</button>
-        <button onClick={declineCookies} className="declineBtn" >Decline</button>
-      </div>
-    </div>
-  );
+    return (
+        <>
+            <div className="cookieContainer">
+                {/* <h1 className="cookie-heading">Wellcome To Cave Counselling</h1> */}
+                <div className="cookie-icon-parent">
+                    <RxCookie />
+                </div>
+                <p className="cookie-text">By using this website, you agree to our use of cookies. We use cookies to provide you with a great experience and to help our website run efficiently.</p>
+                <div className="cookie-btn-container" >
+                    <button onClick={declineCookies} className="declineBtn" >Decline</button>
+                    <button onClick={acceptCookies} className="acceptBtn" >Accept</button>
+                </div>
+            </div>
+        </>
+    );
 };
 
 export default CookieConsent;
